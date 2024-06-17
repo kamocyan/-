c3056058f212d7db055dd4380f564b627f1e1e68
kamocyan <165718127+kamocyan@users.noreply.github.com>
2024-06-17 21:16:05 +0900

    Add files via upload

--- 模写コーディング/index.js
+++ 模写コーディング/index.js
@@ -1,4 +1,4 @@
-$(function () {
+$(document).ready(function(){
     // slickを利用してカルーセルを実装する
     $('.slider').slick({
       arrows: false,
@@ -8,13 +8,3 @@ $(function () {
       speed: 1500,
       pauseOnHover: false
     });
-
-      // リンクのホバー時に不透明度をアニメーションで変更する
-  $('a').hover(
-    function () {
-      $(this).animate({ 'opacity': 0.6 }, 300);
-    },
-    function () {
-      $(this).animate({ 'opacity': 1.0 }, 300);
-    }
-  );
