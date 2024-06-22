90966684e3e0aa5928d04fe0d0323fc404b0b9eb
kamocyan <165718127+kamocyan@users.noreply.github.com>
2024-06-22 23:26:46 +0900

    Add files via upload

---
+++ 練習/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.model').slick({
+
+    });
+
+    $('.close').click(function() {
+        $('#model').hide();
+    });
+    
+    $('.model-content').click(function() {
+        $('#model').show();
+    });
+})
+
